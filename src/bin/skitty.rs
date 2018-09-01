96df78b548c685853d57e61c4120377655deedf0
Daniel Mason <daniel@danielmason.com>
2018-09-01 13:16:20 +0100

    Added: Extract sketch files

--- src/bin/skitty.rs
+++ src/bin/skitty.rs
@@ -1,11 +1,14 @@
 extern crate skitty;
 
-use skitty::watch;
+use skitty::{watch, zip_to_dir};
 
 fn main() {
     let path = std::env::current_dir().expect("current dir");
     let path = path.to_str().expect("to str");
-    if let Err(e) = watch(path) {
+
+    let zip = "/Users/danielmason/projects/rust/skitty/example/HelloWorld.sketch";
+    let dir = "/Users/danielmason/projects/rust/skitty/example/HelloWorld";
+    if let Err(e) = zip_to_dir(zip, dir) {
         println!("error: {:?}", e)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,47 @@
 extern crate notify;
+extern crate zip;
 
 use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use zip::{read::ZipFile, ZipArchive};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::path::Path;
+use std::fs::{File, create_dir_all};
+use std::io::{copy, prelude::*};
+
+fn zipfile_to_file<T>(mut zipfile: ZipFile, dir: T)
+    where T: AsRef<Path>
+{
+    // Test Path is acceptable
+    let file_path = dir.as_ref().join(zipfile.sanitized_name());
+
+    if !file_path.to_string_lossy().contains(dir.as_ref().to_string_lossy().as_ref()) {
+        panic!("zip contained path traversal");
+    }
+
+    let _ = create_dir_all(file_path.parent().expect("Could not open dir"));
+
+    let mut file = File::create(&file_path).expect("Could not create file");
+    let r = copy(&mut zipfile, &mut file);
+    println!("{}", file_path.to_string_lossy());
+}
+
+pub fn zip_to_dir<T, U>(from: T, to: U) -> zip::result::ZipResult<()>
+    where T: AsRef<Path>,
+          U: AsRef<Path>
+{
+    create_dir_all(to.as_ref());
+    let file = File::open(from.as_ref()).expect("Couldn't open file");
+    let mut zip = ZipArchive::new(file)?;
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i).unwrap();
+        zipfile_to_file(file, &to);
+    }
+    Ok(())
+}
 
 pub fn watch<T>(path: T) -> notify::Result<()>
-where T: AsRef<str>
+    where T: AsRef<str>
 {
     // Create a channel to receive the events.
     let (tx, rx) = channel();
