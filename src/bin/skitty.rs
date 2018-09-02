fda3bd3adcbc4ba3e8e6f896dc62f0c1c53ce7d2
Daniel Mason <daniel@danielmason.com>
2018-09-02 10:39:54 +0100

    Added: Clap

--- src/bin/skitty.rs
+++ src/bin/skitty.rs
@@ -1,14 +1,50 @@
+#[macro_use]
+extern crate clap;
+
 extern crate skitty;
 
-use skitty::{watch, dir_to_zip};
+use clap::App;
+
+use skitty::watch;
 
 fn main() {
-    let path = std::env::current_dir().expect("current dir");
-    let path = path.to_str().expect("to str");
+    let yaml = load_yaml!("skitty.yaml");
+    let mut app = App::from_yaml(yaml)
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"));
+    let matches = app.clone().get_matches();
 
-    let zip = "/Users/danielmason/projects/rust/skitty/example/HelloWorld2.sketch";
-    let dir = "/Users/danielmason/projects/rust/skitty/example/HelloWorld";
-    if let Err(e) = dir_to_zip(dir, zip) {
-        println!("error: {:?}", e)
+    match matches.subcommand() {
+        ("watch", Some(matches)) => {
+            if let Some(file) = matches.value_of("FILE") {
+                watch(file);
+            } else {
+                eprintln!("You must specify a file or directory to watch");
+                std::process::exit(1);
+            }
+        },
+        _ => {
+            app.print_help();
+        },
     }
+
+//    if let Some(matches) = matches.subcommand_matches("watch") {
+//        if matches.is_present("FILE") {
+//            println!("Printing debug info...");
+//        } else {
+//            eprintln!("Directory watching is not yet supported");
+//            std::process::exit(1);
+//        }
+//    }
+
+
+//    let path = std::env::current_dir().expect("current dir");
+//    let path = path.to_str().expect("to str");
+//
+//    let zip = "/Users/danielmason/projects/rust/skitty/example/hello-world.sketch";
+//    let dir = "/Users/danielmason/projects/rust/skitty/example/hell0-world";
+//    if let Err(e) = dir_to_zip(dir, zip) {
+//        println!("error: {:?}", e)
+//    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -86,25 +86,26 @@ fn write_file_to_zip<T, U>(zip: &mut ZipWriter<File>, file_path: T, root_path: U
 }
 
 pub fn watch<T>(path: T) -> notify::Result<()>
-    where T: AsRef<str>
+    where T: AsRef<Path>
 {
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
-
-    // Automatically select the best implementation for your platform.
-    // You can also access each implementation directly e.g. INotifyWatcher.
-    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2))?;
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
-
-    // This is a simple loop, but you may want to use more complex logic here,
-    // for example to handle I/O.
-    loop {
-        match rx.recv() {
-            Ok(event) => println!("{:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
-        }
-    }
+    println!("{:#?}", path.as_ref()); Ok(())
+//    // Create a channel to receive the events.
+//    let (tx, rx) = channel();
+//
+//    // Automatically select the best implementation for your platform.
+//    // You can also access each implementation directly e.g. INotifyWatcher.
+//    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2))?;
+//
+//    // Add a path to be watched. All files and directories at that path and
+//    // below will be monitored for changes.
+//    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+//
+//    // This is a simple loop, but you may want to use more complex logic here,
+//    // for example to handle I/O.
+//    loop {
+//        match rx.recv() {
+//            Ok(event) => println!("{:?}", event),
+//            Err(e) => println!("watch error: {:?}", e),
+//        }
+//    }
 }
